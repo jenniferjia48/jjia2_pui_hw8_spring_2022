26392e928e69c9b3bbd4265f89544098db5868c9
Jennifer Jia <jjia2@andrew.cmu.edu>
2022-04-17 01:11:32 -0400

    adding index.html, initial javascript, css, and assets

---
+++ Javascript/main.js
@@ -0,0 +1,19 @@
+//function to change image to signature logo, homepage
+function switchHeaderGraphic() {
+    document.getElementById("headerimage").src = "Assets/jennifer-jia-signature.png";
+}
+
+//function to change image to consulting graphic, homepage
+function switchHeaderGraphicConsulting() {
+    document.getElementById("headerimage").src = "Assets/consulting-graphic.png";
+}
+
+//function to change image to design graphic, homepage
+function switchHeaderGraphicDesign() {
+    document.getElementById("headerimage").src = "Assets/design-graphic.png";
+}
+
+//function to change image to storytelling graphic, homepage
+function switchHeaderGraphicStorytelling() {
+    document.getElementById("headerimage").src = "Assets/storytelling-graphic.png";
+}
